066d4556187cd13b376b3e88cc3fa8e8d4d8238b
Ilyaant <antonov.ia240701@gmail.com>
2023-03-11 23:13:54 +0300

    Added comments

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import PySimpleGUI as sg
 
 
+# Функция для вывода окна админа
 def admin_window():
     layout_admin = [
         [sg.Text('Admin')]
@@ -8,6 +9,7 @@ def admin_window():
     return sg.Window('Клининговая компания. Администратор', layout_admin)
 
 
+# Функция для вывода окна пользователя
 def user_window():
     layout_user = [
         [sg.Text('User')]
@@ -15,6 +17,7 @@ def user_window():
     return sg.Window('Клининговая компания. Пользователь', layout_user)
 
 
+# Функция для вывода окна сотрудника
 def worker_window():
     layout_worker = [
         [sg.Text('Worker')]
@@ -22,8 +25,9 @@ def worker_window():
     return sg.Window('Клининговая компания. Пользователь', layout_worker)
 
 
-sg.theme('sandy beach')
+sg.theme('sandy beach')  # цветовая тема приложения
 
+# интерфейс главного окна
 layout = [
     [sg.Text('Пожалуйста, выполните вход')],
     [sg.Text('Логин:'), sg.InputText(key='-LOGIN-')],
@@ -31,15 +35,20 @@ layout = [
     [sg.Button('Войти'), sg.Push(), sg.Button('Выход')]
 ]
 
-window = sg.Window('Клининговая компания. Вход', layout)
+window = sg.Window('Клининговая компания. Вход',
+                   layout)  # открытие главного окна
 
 while True:
-    event, values = window.read()
+    event, values = window.read()  # отслеживание состояния и переменных главного окна
 
+    # обработка события выхода из приложения
     if event == sg.WINDOW_CLOSED or event == 'Выход':
         break
 
+    # обработка события нажатия на кнопку "Войти"
     if event == 'Войти':
+
+        # запуск окна админа (проверка логина и пароля admin)
         if values['-LOGIN-'] == 'admin' and values['-PASS-'] == 'admin':
             window_admin = admin_window()
             while True:
@@ -48,6 +57,7 @@ while True:
                     break
             window_admin.close()
 
+        # запуск окна пользователя (проверка логина и пароля user)
         elif values['-LOGIN-'] == 'user' and values['-PASS-'] == 'user':
             window_user = user_window()
             while True:
@@ -56,6 +66,7 @@ while True:
                     break
             window_user.close()
 
+        # запуск окна работника (проверка логина и пароля worker)
         elif values['-LOGIN-'] == 'worker' and values['-PASS-'] == 'worker':
             window_worker = worker_window()
             while True:
@@ -64,7 +75,8 @@ while True:
                     break
             window_worker.close()
 
+        # обработка ошибки ввода неизвестного логина и пароля
         else:
             sg.Popup('Ошибка. Проверьте введенные данные', title='Ошибка')
 
-window.close()
+window.close()  # закрытие главного окна
